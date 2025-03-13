ea068f92b570ea47499739a0c17bab42647ff36c
Anshuman Pratap Singh <anshuman.pratap@jagrannewmedia.com>
2025-03-13 11:07:40 +0530

    update the config

--- config/config.js
+++ config/config.js
@@ -9,5 +9,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/liveOdds",
-  JWT_SECRET: process.env.JWT_SECRET || "default_secret",
+  //JWT_SECRET: process.env.JWT_SECRET || "default_secret",
 };
